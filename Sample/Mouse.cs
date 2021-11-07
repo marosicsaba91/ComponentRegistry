1183044cb9db135e4070ac3ca1dafac25bc6c7a1
Marosi Csaba <marosicsaba91@gmail.com>
2021-11-07 13:49:45 +0100

    fix

--- Sample/Mouse.cs
+++ Sample/Mouse.cs
@@ -1,6 +1,9 @@
-﻿﻿public interface ICatFood : IFood<Cat> { }
+﻿using System.Collections.Generic;
+using ComponentRegistrySystem; 
+
+public interface ICatFood : IFood<Cat> { }
 
 public sealed class Mouse : Animal, ICatFood
 { 
-    public override string Introduce() => "I'm Afraid!!!";
+    public override string Introduce() => "I'm Afraid!!!"; 
 }
