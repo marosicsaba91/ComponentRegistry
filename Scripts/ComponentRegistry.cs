fb0c6573109ca05aff3aba295de4cb59a8ea61bf
Csaba Marosi <marosicsaba91@gmail.com>
2022-02-27 19:50:45 +0100

    Packages

--- Scripts/ComponentRegistry.cs
+++ Scripts/ComponentRegistry.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq; 
-using MarosiUtility;
+using MUtility;
 using UnityEngine;
 using UnityEngine.SceneManagement;
  using MUtility;
